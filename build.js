dc211cd4a2cdc095dc09e4156c0518a285d1dfe9
jeff-hykin <jeff.hykin@gmail.com>
2020-10-19 20:58:49 -0500

    -

--- build.js
+++ build.js
@@ -17,7 +17,7 @@ let commits = allCommitsInCwd()
 let [start, end] = [ (process.argv[3]||0)-0, (process.argv[4]||commits.length)-0]
 commits = commits.slice(start, end)
 
-// took 1d 21h 31m 40s to run this though 24392 commits
+// took 45h 31m 40s to run this though 24392 commits
 let packages = {}
 for (let [index, eachCommit] of Object.entries(commits)) {
     index = index-0
