3956180e28dd59a67a1d487ec947493237a3f20d
Jeff Hykin <jeff.hykin@gmail.com>
2022-05-15 17:08:04 -0500

    search page functions

---
+++ generate_multi_repos.js
@@ -0,0 +1,38 @@
+const { run, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo, zipInto, mergeInto, returnAsString, } = await import(`https://deno.land/x/quickr@0.3.24/main/run.js`)
+const { FileSystem } = await import(`https://deno.land/x/quickr@0.3.24/main/file_system.js`)
+const { Console, yellow } = await import(`https://deno.land/x/quickr@0.3.24/main/console.js`)
+
+import { MeiliSearch } from "https://cdn.skypack.dev/meilisearch"
+import { hashJsonPrimitive } from "./tools.js"
+
+const client = new MeiliSearch({ host: 'http://127.0.0.1:7700' })
+
+
+function addToDatabase(packageObject) {
+    const id = hashJsonPrimitive(packageObject.frozen)
+    // dont need to wait on this
+    return client.index('packages').updateDocuments([
+        {
+            ...allPackages[packageName][hashValue],
+            id: hashValue,
+        }
+    ])
+}
+
+const allPackagePaths = await FileSystem.listFolderPathsIn(`scan/packages`)
+for (const eachPath of allPackagePaths) {
+    const packageName = FileSystem.basename(allPackagePaths)
+    // FIXME: create a repo
+    
+    FileSystem.write({ path:  })
+    if (eachPath.slice(-4) !== '.json') {
+        continue
+    }
+    const jsonString = await FileSystem.read(eachPath)
+    if (jsonString) {
+        const value = JSON.parse(jsonString)
+        if (value && value.frozen instanceof Object) {
+            addToDatabase(value)
+        }
+    }
+}
